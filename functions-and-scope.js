a10df68de12ecc0217c00d8d390809feb340845e
El-Sankey <i.a.rivashernandez@gmail.com>
2025-04-17 08:56:14 +0200

    funtion-&-sscops opdr. 2 af

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -1,18 +1,30 @@
+
 // Je gaat functies schrijven die we kunnen hergebruiken om een lijst met eindcijfers van studenten te checken. Je zult over de cijfers heen moeten itereren (hoe pak je dat aan?),
 // maar ook een manier moeten vinden om hetgeen dat je verzamelt ergens te bundelen. Op deze manier zul je ontdekken hoe je omgaat met scope. Pak vooral het hoofdstuk op EdHub over for-loops er nog eens bij!
 // Tip: je mag hier geen ingebouwde object methoden gebruiken, dus daar hoef je niet naar te kijken.
 
 const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
-/* Opdracht  1: Cum Laude */
+                                            /* Opdracht  1: Cum Laude */
 
 /* 1a: Script schrijven  */
 // De administratie moet weten hoeveel studenten er dit blok cum laude zijn afgestudeerd (8 of hoger). Daar moeten namelijk speciale diploma's voor besteld worden.
 // Schrijf de stapjes om dit te kunnen checken eerst uit en vraag jezelf de volgende dingen af:
-// * Hoe kan ik iedere waarde van de array checken op deze conditie?
 // * Hoe zorg ik ervoor dat dit ook werkt wanneer de array 100 entries bevat?
+// * Hoe kan ik iedere waarde van de array checken op deze conditie?
 // * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan bijhouden?
 // Log het antwoord in de terminal.
+
+//
+// let cumLaud = 0
+// for (let i = 0; i < grades.length; i++) {
+//     if (grades[i] >=8){
+//         cumLaud = cumLaud + 1
+//     }
+//
+// }
+
+// console.log('cumLaud')
 
 // ---- Verwachte uitkomst: 6
 
@@ -22,23 +34,62 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Zorg ervoor dat jouw functie ook werkt als we een andere array met eindcijfers willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
 // Log het antwoord in de terminal.
 
+
+function cumLaud1(cijfers) {
+    let cumLaud = 0
+    for (let i = 0; i < cijfers.length; i++) {
+        if (cijfers[i] >=8) {
+            cumLaud = cumLaud + 1
+        }
+    }
+    return cumLaud;
+}
+
+
+// console.log(cumLaud1())
+
+
 // ---- Verwachte uitkomsten:
 // cumLaude(grades) geeft 6
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
 
+console.log(cumLaud1(grades));
+console.log(cumLaud1([6, 4, 5]));
+console.log(cumLaud1([8, 9, 4, 6, 10]));
 
 
-/* Opdracht  2: Gemiddeld cijfer */
+
+                                        /* Opdracht  2: Gemiddeld cijfer */
 
 /* 2a: Script schrijven  */
 // De studenten-administratie moet ieder blok opnieuw berekenen wat het gemiddelde eindcijfer is, maar we beginnen met de grades array van hierboven.
 // Schrijf de stapjes om dit te kunnen berekenen eerst uit en vraag jezelf de volgende dingen af:
 // * Hoe wordt een gemiddelde berekend?
+
+// let total = 0
+// for (let cijfers of grades) {
+//     total = cijfers + grades
+// }
+// let gemiddeldCijfer;
+// gemiddeldCijfer = total / grades.length;
+// console.log(gemiddeldCijfer)
+
+// for (let i = 0; i < grades; i++) {
+//     if ( i / grades) {
+//
+//     }
+//
+// }
+
+// console.log()
+
 // * Wat moet ik verzamelen uit de array van cijfers om uiteindelijk een gemiddelde te kunnen berekenen?
 // * Hoe zorgt ik ervoor dat ik alle waardes uit de array kan langslopen, ook als de array wel 100 entries zou bevatten?
 // Log het antwoord in de terminal.
+
+
 
 // ---- Verwachte uitkomst: 6.642857142857143
 
@@ -63,7 +114,8 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 /* Bonusopdracht: hoogste cijfer */
 
-/* 3a: Script schrijven  */
+                                            /* 3a: Script schrijven  */
+
 // Schrijf een script die op basis van de grades array (hierboven) checkt wat het hoogst behaalde cijfer is. Je mag hier geen bestaande methoden voor gebruiken. Schrijf de stapjes eerst uit en vraag jezelf de volgende dingen af:
 // * Hoe kan ik iedere waarde van de array langsgaan?
 // * Op welke conditie moet ik checken?
